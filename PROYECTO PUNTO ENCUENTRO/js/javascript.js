7bd437582981849c4a28b4115d89d6b2f98bdceb
Ainhoa <aquigon2002@g.educaand.es>
2025-03-31 18:04:41 +0200

    Fix-Js-AQG

--- PROYECTO PUNTO ENCUENTRO/js/javascript.js
+++ PROYECTO PUNTO ENCUENTRO/js/javascript.js
@@ -5,25 +5,27 @@ const idiomaListaDiv = document.getElementById("idiomaListaDiv");
 
 
 
-botonAñadeIdiomas.addEventListener("click", function(){
+botonAñadeIdiomas.addEventListener("click", function(event){
 
+    //Gracias a esto lo que creemos se quede todo el rato
+    event.preventDefault();
     //Cogemos que es lo que se ha seleccionado en el select
     const idiomaSeleccionado = document.getElementById("IdiomasSelect").value;
     const nivelIdiomaSeleccionado = document.getElementById("IdiomasNivel").value;
 
     const creacionDiv = document.createElement("div");
+    idiomaListaDiv.classList.add("idiomaListaDiv");
     idiomaListaDiv.appendChild(creacionDiv);
-
+    
     //Creamos los parrafos donde esta la informacion
-    const createParr1 = document.createElement("p");
+    const createParr1 = document.createElement("span");
     creacionDiv.appendChild(createParr1);
     createParr1.classList.add("IdiomaParr");
     createParr1.textContent = idiomaSeleccionado;
 
-    const createParr2 = document.createElement("p");
+    const createParr2 = document.createElement("span");
     creacionDiv.appendChild(createParr2);
     createParr2.classList.add("IdiomaParr");
     createParr2.textContent = nivelIdiomaSeleccionado;
-
 
 })
