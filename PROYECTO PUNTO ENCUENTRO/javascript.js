06e1dd84ad83131da2aba74f378925ff444f3fba
Ainhoa <aquigon2002@g.educaand.es>
2025-03-31 16:18:59 +0200

    enhancement-Js y perfil- AQG

--- PROYECTO PUNTO ENCUENTRO/javascript.js
+++ PROYECTO PUNTO ENCUENTRO/javascript.js
@@ -1,1 +1,20 @@
 /*Boton que añade idiomas */
+const botonAñadeIdiomas = document.getElementById("botonAddLeng");
+const SelectIdiomas = document.getElementById("IdiomasSelect");
+const idiomaListaDiv = document.getElementById("idiomaListaDiv");
+
+
+
+
+botonAñadeIdiomas.addEventListener("click", function(){
+
+    //Cogemos que es lo que se ha seleccionado en el select
+    const idiomaSeleccionado = document.getElementById("IdiomasSelect").value;
+    const nivelIdiomaSeleccionado = document.getElementById("IdiomasNivel").value;
+
+    
+
+
+
+
+})
